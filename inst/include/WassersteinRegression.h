4b8272ceab98e3fe5228efbf4875050137a79efa
juancarlosvidal <juan.carlos.vidal@gmail.com>
2020-10-25 00:39:04 +0200

    Third commit
    Added some authoring comments

--- inst/include/WassersteinRegression.h
+++ inst/include/WassersteinRegression.h
@@ -1,4 +1,22 @@
-// WassersteinRegression.h
+// WassersteinRegression.h: Rcpp/Wass glue
+//
+// Copyright (C) 2019 - 2020  Juan Carlos Vidal and Marcos Matabuena 
+//
+// This file is part of RcppWass.
+//
+// RcppWass is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+//
+// RcppWass is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with RcppWass.  If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef _WASSERSTEIN_REGRESSION_H // include guard
 #define _WASSERSTEIN_REGRESSION_H
 
