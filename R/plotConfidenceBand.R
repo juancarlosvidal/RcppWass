e01fcfec5bc5ccc94818daaee46d4be9b5faac88
juancarlosvidal <juan.carlos.vidal@gmail.com>
2020-10-25 02:39:18 +0200

    Some more tests

--- R/plotConfidenceBand.R
+++ R/plotConfidenceBand.R
@@ -1,21 +1,26 @@
-// plotConfidenceBand.R: Rcpp/Wass glue
-//
-// Copyright (C) 2019 - 2020  Juan Carlos Vidal and Marcos Matabuena 
-//
-// This file is part of RcppWass.
-//
-// RcppWass is free software: you can redistribute it and/or modify it
-// under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 2 of the License, or
-// (at your option) any later version.
-//
-// RcppWass is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with RcppWass.  If not, see <http://www.gnu.org/licenses/>.
+## plotConfidenceBand.R: Rcpp/Wass glue
+##
+## Copyright (C) 2019 - 2020  Juan Carlos Vidal and Marcos Matabuena 
+##
+## This file is part of RcppWass.
+##
+## RcppWass is free software: you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 2 of the License, or
+## (at your option) any later version.
+##
+## RcppWass is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with RcppWass.  If not, see <http://www.gnu.org/licenses/>.
+
+#library(ggplot2)
+#library(cowplot)
+#library(dplyr)
+#library(tidyr)
 
 plot_confidence_band <- function(t, res) {
   Qp <- fdata(res$Qpred, argvals = t)
